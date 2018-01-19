cf8f6ab26985abf73c813337a6429cd98390645c
Patty Kovash <pattykovash@gmail.com>
2018-01-19 00:04:01 -0700

    Add like button

--- react-client/src/components/EatViewListEntry.jsx
+++ react-client/src/components/EatViewListEntry.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 let EatViewListEntry = (props) => {
   return (
     <div key={ props.restaurant.id }>
-      <h3><a href={ props.restaurant.url }>{ props.restaurant.name }</a></h3>
-      <p>{ props.restaurant.location }</p>
+      <h1><a href={ props.restaurant.url }>{ props.restaurant.name }</a></h1>
       <img src={props.restaurant.imageUrl.replace('/o.jpg', '/m.jpg')} alt="" />
+      <button type="button">like</button>
     </div>
   );
 };
@@ -13,11 +13,5 @@ let EatViewListEntry = (props) => {
 export default EatViewListEntry;
 
 
-//   return (
-//     <tr>
-//       <td> <a href={ props.restaurant.url }><h3>{ props.restaurant.name }</h3></a> </td>
-//       <td> <img src={props.item.imageUrl.replace('/o.jpg', '/m.jpg')} alt="" /> </td>
-//     </tr>
-//   );
-// };
+
 
