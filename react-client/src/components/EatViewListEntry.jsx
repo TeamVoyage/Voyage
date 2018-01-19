cbde7ad67ac5d33bef945996c98b149c7f874750
Patty Kovash <pattykovash@gmail.com>
2018-01-19 00:53:05 -0700

    Change props, styling, and add like button

--- react-client/src/components/EatViewListEntry.jsx
+++ react-client/src/components/EatViewListEntry.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 let EatViewListEntry = (props) => {
   return (
     <div key={ props.restaurant.id }>
-      <h1><a href={ props.restaurant.url }>{ props.restaurant.name }</a></h1>
+      <h4><a href={ props.restaurant.url }>{ props.restaurant.name }</a></h4>
       <img src={props.restaurant.imageUrl.replace('/o.jpg', '/m.jpg')} alt="" />
       <button type="button">like</button>
     </div>
--- react-client/src/components/Results.jsx
+++ react-client/src/components/Results.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import EatView from './EatView.jsx';
 import SleepView from './SleepView.jsx';
 import ExploreView from './ExploreView.jsx';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 class Results extends React.Component {
   constructor(props) {
@@ -17,11 +16,11 @@ class Results extends React.Component {
         </div>
         <hr/>
         <div>
-          <SleepView hotels={ this.props.hotels } />
+          <SleepView info={ this.props.info } />
         </div>
         <hr/>
         <div>
-          <ExploreView events={ this.props.events } />
+          <ExploreView info={ this.props.info } />
         </div>
       </div>
     );
@@ -29,31 +28,3 @@ class Results extends React.Component {
 }
 
 export default Results;
-{/*
-        <PartyView />
-      <div>
-        <Link to='/eat'>Eat</Link>
-        <PartyView />
-        <SleepView />
-        <ExploreView />
-      </div>
-
-
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <Link to={'/eat'}>Eat</Link>
-                <Link to={'/party'}>Party</Link>
-                <Link to={'/sleep'}>Sleep</Link>
-                <Link to={'/explore'}>Explore</Link>
-              </div>
-            </div>
-          </nav>
-          <div>
-            <Switch>
-              <Route path="/eat" render={() => <EatView eat={ this.props.eat } />} />
-            </Switch>
-          </div>
-        </div>
-*/}
