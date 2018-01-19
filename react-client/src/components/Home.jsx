675b977490e30ca7d9d4dbbc463f9ed117870435
Patty Kovash <pattykovash@gmail.com>
2018-01-19 00:04:53 -0700

    Add conditional to determine if Board component should render.
    Change props to children

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import SearchLocation from './SearchLocation.jsx';
 import Results from './Results.jsx';
+// import Board from './Board.jsx';  // uncomment when created
 
 
 class Home extends React.Component {
@@ -11,16 +12,28 @@ class Home extends React.Component {
 
   render() {
     console.log('HOME PROPS: ', this.props);
+
+    let displayBoard = null;
+
+    // If user logged in, render <Board /> component
+    if (this.props.info.isLoggedIn) {
+      displayBoard = <Board />;
+    } else {
+      displayBoard = null;
+    }
     return (
       <div>
         <SearchLocation go={ this.props.go }/>
         <hr/>
-
         <Results
-          restaurants={ this.props.categories.restaurants }
-          hotels={ this.props.categories.hotels }
-          events={ this.props.categories.events }
+          info={ this.props.info }
+          // restaurants={ this.props.categories.restaurants }
+          hotels={ this.props.info.hotels }
+          events={ this.props.info.events }
+          // location={ this.props.categories.location }
         />
+        <hr/>
+        { displayBoard }
       </div>
     );
   }
