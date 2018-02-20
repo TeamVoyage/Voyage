83610b736bfca80cfe00cd9fc2a300879592b94d
Daniel Kelly <djk8552@gmail.com>
2018-02-19 22:05:46 -0500

    Modify travis.yml and clean up code

--- database/index.js
+++ database/index.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/tripcollab');
 mongoose.connect(process.env.MONGOLAB_PURPLE_URI);
 
-let restaurantSchema = mongoose.Schema({
+const restaurantSchema = mongoose.Schema({
   name: String,
   imageUrl: String,
   url: String
 });
 
-let hotelSchema = mongoose.Schema({
+const hotelSchema = mongoose.Schema({
   name: String,
   imageUrl: String,
   url: String
 });
 
-let eventSchema = mongoose.Schema({
+const eventSchema = mongoose.Schema({
   name: String,
   imageUrl: String,
   url: String
 });
 
-let attractionSchema = mongoose.Schema({
+const attractionSchema = mongoose.Schema({
   name: String,
   imageUrl: String,
   url: String
 });
 
-let userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   googleId: String,
   username: String,
   trips: Array
 });
 
-let tripSchema = mongoose.Schema({
+const tripSchema = mongoose.Schema({
   name: String,
   location: String,
   restaurants: [restaurantSchema],
@@ -44,27 +44,7 @@ let tripSchema = mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 const Trip = mongoose.model('Trip', tripSchema);
 
-// // dummy data for testing
-// const testTrip = {
-//   name: 'example trip2',
-//   location: 'chicago',
-//   restaurants: [{name: 'Burger King', price: 1, rating: 3 }],
-//   hotels: [{name: 'Hyatt', price: 2, rating: 3 }],
-//   events: [{name: 'Some Comedy Show'}],
-//   attractions: [{name: 'Space Musuem'}]
-// };
-//
-// const testUser = new User({
-//   googleId: 'daniel',
-//   username: 'kelly',
-//   trips: []
-// });
-//
-// testUser.save(err => {
-//   if(err) console.log(err);
-// });
-
-let saveTrip = (tripInfo, cb) => {
+const saveTrip = (tripInfo, cb) => {
   const trip = new Trip({
     name: tripInfo.name,
     location: tripInfo.location,
@@ -97,18 +77,15 @@ let saveTrip = (tripInfo, cb) => {
   // });
 };
 
-let getAllTrips = (cb) => {
-    Trip.find({}, (err, trips) => {
-      if (err) {
-        return handleError(err);
-      } else {
-        cb(trips);
-      }
-    });
-  };
-
-// // test db functionality
-// saveTrip(testTrip2, 'daniel');
+const getAllTrips = (cb) => {
+  Trip.find({}, (err, trips) => {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(trips);
+    }
+  });
+};
 
 module.exports.saveTrip = saveTrip;
 module.exports.getAllTrips = getAllTrips;
